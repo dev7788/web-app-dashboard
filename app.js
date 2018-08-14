2adc4b11bd2a1a491e2c6f261648f25485c66288
dev7788 <devvv7788@gmail.com>
2018-08-13 20:44:44 -0400

    Added alert button script

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // Global variables
+const alert = document.getElementById('alert');
+const alertButton = document.getElementById('alert-button');
+
+// Hides the alert message when button is clicked
+alertButton.addEventListener('click', (e) => {
+  alert.style.display = 'none';
+});
