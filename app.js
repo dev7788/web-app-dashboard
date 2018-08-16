46f58bf90fc19823377943cbb64f7bfe3dec6c7f
dev7788 <devvv7788@gmail.com>
2018-08-15 22:24:04 -0400

    Added more display tab styling

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // Global variables
+const alertDot = document.getElementById('header-right-dot');
 const alert = document.getElementById('alert');
 const alertButton = document.getElementById('alert-button');
 
-// Hides the alert message when button is clicked
+// Hides the alert dot and message when button is clicked
 alertButton.addEventListener('click', (e) => {
-  alert.style.display = 'none';
+  alertDot.style.visibility = 'hidden';
+  alertDot.style.opacity = '0';
+  alert.style.visibility = 'hidden';
+  alert.style.opacity = '0';
 });
